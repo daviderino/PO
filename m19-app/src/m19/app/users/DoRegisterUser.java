dd88c10c378d6da4ece597a541307535da404f28
David <david.baptista00@hotmail.com>
2019-11-11 20:22:13 +0000

    yeeess

--- m19-app/src/m19/app/users/DoRegisterUser.java
+++ m19-app/src/m19/app/users/DoRegisterUser.java
@@ -4,6 +4,7 @@ package m19.app.users;
 // FIXME import ui concepts
 
 import m19.LibraryManager;
+import m19.User;
 import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.DialogException;
 import pt.tecnico.po.ui.Input;
@@ -13,23 +14,24 @@ import pt.tecnico.po.ui.Input;
  */
 public class DoRegisterUser extends Command<LibraryManager> {
 
-  private Input<String> _name;
-  private Input<String> _email;
+	private Input<String> _name;
+	private Input<String> _email;
 
-  /**
-   * @param receiver
-   */
-  public DoRegisterUser(LibraryManager receiver) {
-    super(Label.REGISTER_USER, receiver);
-    _name = _form.addStringInput(Message.requestUserName());
-    _email = _form.addStringInput(Message.requestUserEMail());
-  }
+	/**
+	 * @param receiver
+	 */
+	public DoRegisterUser(LibraryManager receiver) {
+		super(Label.REGISTER_USER, receiver);
+		_name = _form.addStringInput(Message.requestUserName());
+		_email = _form.addStringInput(Message.requestUserEMail());
+	}
 
-  /** @see pt.tecnico.po.ui.Command#execute() */
-  @Override
-  public final void execute() throws DialogException {
-    _form.parse();
-    
-  }
+	/** @see pt.tecnico.po.ui.Command#execute() */
+	@Override
+	public final void execute() throws DialogException {
+		_form.parse();
+		int id =  _receiver.createUser(_name.value(), _email.value());
+		_display.popup(Message.userRegistrationSuccessful(id));
+	}
 
 }
